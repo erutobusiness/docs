77362296fdcea756bb3c8704adfb302815b0cbbc
Eruto Kondo <erutobusiness@gmail.com>
2025-05-07 23:03:46 +0900

    package-lock.jsonにライセンス情報を追加し、tailwind.config.jsのpurge設定を修正

--- declarative/next/tailwind.config.js
+++ declarative/next/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './src/app/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: { extend: {} },
   plugins: [],
 };
