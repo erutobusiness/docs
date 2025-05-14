4bc41cd19868ef67b063a6681b0933aee233add4
Eruto Kondo <erutobusiness@gmail.com>
2025-05-14 23:49:15 +0900

    わかた！basePathがミスってたんだ

--- declarative/next/next.config.ts
+++ declarative/next/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', // 静的HTMLにエクスポート
-  basePath: process.env.NODE_ENV === 'production' ? '/docs/declarative' : '', // GitHubリポジトリ名を指定
+  basePath: process.env.NODE_ENV === 'production' ? '/docs/' : '',
   trailingSlash: true, // URLの末尾にスラッシュを追加
   images: {
     remotePatterns: [
