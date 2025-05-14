57d8015c295b8fdade39d8998a925babbc7aaac8
Eruto Kondo <erutobusiness@gmail.com>
2025-05-14 23:51:10 +0900

    最後にスラッシュはいらない？

--- declarative/next/next.config.ts
+++ declarative/next/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export', // 静的HTMLにエクスポート
-  basePath: process.env.NODE_ENV === 'production' ? '/docs/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/docs' : '',
   trailingSlash: true, // URLの末尾にスラッシュを追加
   images: {
     remotePatterns: [
