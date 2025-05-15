1d7dcebcc0649cd532600d56bfe1b5875bcc812e
Eruto Kondo <erutobusiness@gmail.com>
2025-05-15 19:15:59 +0900

    限定共有記事にOrganizationを紐付けることはできません

--- scripts/qiita-concat-declarative.js
+++ scripts/qiita-concat-declarative.js
@@ -17,7 +17,7 @@ const config = {
   private: true,
   updated_at: '',
   id: '',
-  organization_url_name: 'visionary-jp',
+  organization_url_name: '',
   slide: false,
   ignorePublish: false,
 };
