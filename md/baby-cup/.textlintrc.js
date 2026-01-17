82a48520f1784d85c718ce8c09388cdf6dbc5973
Eruto Kondo <erutobusiness@gmail.com>
2026-01-17 13:46:38 +0900

    feat: baby-cupドキュメントの初期コンテンツとtextlint設定を追加

--- md/baby-cup/.textlintrc.js
+++ md/baby-cup/.textlintrc.js
@@ -8,9 +8,9 @@ module.exports = {
       ...baseConfig.rules['preset-ja-technical-writing'],
       'no-mix-dearu-desumasu': {
         ...baseConfig.rules['preset-ja-technical-writing']['no-mix-dearu-desumasu'],
-        preferInBody: 'である',
-        preferInHeader: 'である',
-        preferInList: 'である',
+        preferInBody: 'ですます',
+        preferInHeader: 'ですます',
+        preferInList: 'ですます',
       },
     },
   },
